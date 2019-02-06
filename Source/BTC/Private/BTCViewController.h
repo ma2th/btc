81fcadbdacecabff32b77c90e3d4093565d8a258
Matthias <matthias.ring@cs.fau.de>
2019-02-06 14:38:32 +0100

    #ifdef inserted

--- Source/BTC/Private/BTCViewController.h
+++ Source/BTC/Private/BTCViewController.h
@@ -14,6 +14,7 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
+#ifdef PLATFORM_IOS
 #if PLATFORM_IOS
 
 #include "Queue.h"
@@ -55,4 +56,5 @@
 
 @end
 
+#endif
 #endif
--- Source/BTC/Private/BTCViewController.mm
+++ Source/BTC/Private/BTCViewController.mm
@@ -14,6 +14,7 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
+#ifdef PLATFORM_IOS
 #if PLATFORM_IOS
 
 #include "BTCConfiguration.h"
@@ -207,4 +208,5 @@
 
 @end
 
+#endif
 #endif
